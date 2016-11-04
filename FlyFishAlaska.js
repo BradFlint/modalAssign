06e69ecdfd2d55a6c06eb2efa613fc153d2b4ab9
Brad Flint <bradwflint@gmail.com>
2016-11-04 16:19:37 -0600

    second commit

--- FlyFishAlaska.js
+++ FlyFishAlaska.js
@@ -1,3 +1,5 @@
+// display modal, set the amount of time before it appears to 3 seconds
+
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
@@ -7,6 +9,8 @@ function display() {
 }
 
 setTimeout(display, 3000);
+
+// gives the option to close modal with clicking on the x button
 
 span.addEventListener('click', function() {
     modal.style.display = "none";
